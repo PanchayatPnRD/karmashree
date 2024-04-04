58a64730c74d804e9f75cb6a1c8c0d9114a06516
animesh <cy.animesh@gmail.com>
2024-04-04 11:24:43 +0530

    update test

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect} from 'react'
 
 const Contact = () => {
-  return <div className="flex-grow overflow-auto text-center ">Contact Us</div>;
+  return <div className="flex-grow overflow-auto text-center ">Contact ffUs</div>;
 }
 
 export default Contact
