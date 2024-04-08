01649a27df72e25601820e6b650af950e5af145e
animesh <cy.animesh@gmail.com>
2024-04-08 10:43:10 +0530

    update

--- src/views/forms/DashboardHome.jsx
+++ src/views/forms/DashboardHome.jsx
@@ -1,9 +1,44 @@
+import { useState, useEffect} from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const DashboardHome = () => {
   return (
-    <div className="flex flex-grow justify-center items-center text-4xl">
-      This is dashboard home
+    <div className="flex-grow">
+      <ToastContainer />
+      <div className="mx-auto mt-2">
+        <div className="bg-white rounded-lg p-12">
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+            {/* Mini Card 1 */}
+            <div className="bg-blue-200 rounded-lg p-4 flex flex-col justify-center items-center">
+              <h3 className="text-lg font-semibold mb-2">Total Scheme</h3>
+              <p className="text-xl font-bold">1</p>
+            </div>
+
+            {/* Mini Card 2 */}
+            <div className="bg-green-200 rounded-lg p-4 flex flex-col justify-center items-center">
+              <h3 className="text-lg font-semibold mb-2">Total Contractor</h3>
+              <p className="text-xl font-bold">2</p>
+            </div>
+
+            {/* Mini Card 3 */}
+            <div className="bg-yellow-200 rounded-lg p-4 flex flex-col justify-center items-center">
+              <h3 className="text-lg font-semibold mb-2">Total Action Plan</h3>
+              <p className="text-xl font-bold">3</p>
+            </div>
+
+            {/* Mini Card 4 */}
+            <div className="bg-pink-200 rounded-lg p-4 flex flex-col justify-center items-center">
+              <h3 className="text-lg font-semibold mb-2">Employment</h3>
+              <p className="text-xl font-bold">4</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
+
 
 export default DashboardHome;
--- src/views/forms/NewUser.jsx
+++ src/views/forms/NewUser.jsx
@@ -227,6 +227,32 @@ const onDesignation=(e)=>{
     <div className="flex-grow ">
       <ToastContainer />
       <div className="mx-auto mt-2">
+      <div className="bg-white rounded-lg p-12">
+  <div className="shadow-md" style={{ marginBottom: '-1rem' }}>
+    <div className="flex justify-between items-center">
+      <div className="flex items-center space-x-4">
+        <nav aria-label="Breadcrumb">
+          <ol className="flex items-center space-x-4 px-4 py-2"> {/* Added padding */} <svg
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+ 
+    >
+      <path d="M946.5 505L534.6 93.4a31.93 31.93 0 00-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z" />
+    </svg>
+            <li>
+              <a href="#" className="text-indigo-600 hover:text-indigo-800">Home</a>/
+            </li>
+            <li className="text-gray-500 font-bold" aria-current="page">User Registration</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+    <br />
+  </div>
+
+</div>
         <div className="flex w-full space-x-4">
           <div>
             <label
