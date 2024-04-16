e165fcb771042b012d7680f7d31387baf3f5dfa2
Soumyadeep Ghosh <thekillermax1999@gmail.com>
2024-04-16 13:00:53 +0530

    modal added for user creation

---
+++ src/components/SuccessModal.jsx
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import classNames from "classnames";
+import { Modal } from "flowbite-react";
+
+const SuccessModal = ({ setOpenModal, openModal, isSuccess, errorMsg }) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Body className="px-0 pt-0">
+          <h1
+            className={classNames(
+              "text-center py-6 text-2xl text-white font-semibold border-b-2 rounded-t-lg",
+              isSuccess ? "bg-green-400" : "bg-red-400"
+            )}
+          >
+            {isSuccess ? "Success" : "Error"}
+          </h1>
+          <div className="space-y-6 px-12 p-6">
+            {isSuccess ? (
+              <>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  You have successfully been registered in Karmashree portal ,
+                  your user id is __ and details have been sent to your
+                  registered mobile number
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  Please change your password in first login
+                </p>
+              </>
+            ) : (
+              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                {errorMsg}
+              </p>
+            )}
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="flex justify-center items-center">
+          <button
+            className="relative animate-pulse2 bg-blue-600 p-2 px-6 rounded-lg text-white font-semibold hover:shadow-md transition-all"
+            onClick={() => {
+              setOpenModal((prev) => !prev);
+              isSuccess && navigate("/dashboard/dept-userlist", { state: "dept-user" });
+            }}
+          >
+            {/* <div className="absolute -top-2 -right-2 size-5 rounded-full bg-red-600 animate-ping"></div>
+            <div className="absolute -top-1 -right-1 size-3 rounded-full bg-red-600"></div>
+            <span>Okay</span> */}
+            Okay
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default SuccessModal;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        pulse2: "pulse2 1.3s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
       backgroundImage: {
         banner: "url('/assets/img/mamta_banner_bg.png')",
         curve: "url('/assets/img/curve_border2.png')",
@@ -16,6 +19,12 @@ export default {
       },
       minHeight: {
         remaining: "calc(100vh - 98px)", // Adjust the '4rem' value
+      },
+      keyframes: {
+        pulse2: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: .7 },
+        },
       },
     },
   },
