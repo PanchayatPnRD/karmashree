bea9b9986c6047ce2a06ebaf3db6998f6f619af7
Soumyadeep Ghosh <thekillermax1999@gmail.com>
2024-05-02 22:13:27 +0530

    fixed scroll issue with action plan list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
           </div>
           <div className="flex flex-col -space-y-1">
             <h1 className="capitalize font-semibold text-2xl tracking-tight">
-              department of panchayat & rural development
+              department of panchayats & rural development
             </h1>
             <h3 className="capitalize text-zinc-500">
               government of west bengal
