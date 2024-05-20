3a0273e3046865f5f1ae9e50e5bd9d6971ac5661
Soumyadeep Ghosh <thekillermax1999@gmail.com>
2024-05-20 15:24:36 +0530

    Update SchemeList.jsx

--- src/views/forms/SchemeList.jsx
+++ src/views/forms/SchemeList.jsx
@@ -2,20 +2,21 @@ import { useState, useEffect, useMemo } from "react";
 import { Table } from "flowbite-react";
 import { useQuery } from "@tanstack/react-query";
 import { fetch } from "../../functions/Fetchfunctions";
-
+import { SortIcon } from "../../components/SortIcon";
+import { Pagination } from "../../components/Pagination";
+import classNames from "classnames";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { getAllSchemeList } from "../../Service/Scheme/SchemeService";
 
 const SchemeList = () => {
-  const [currentPage, setCurrentPage] = useState(1);
   const { userIndex } = JSON.parse(localStorage.getItem("karmashree_User"));
-  const [startIndex, endIndex] = useMemo(() => {
-    const start = (currentPage - 1) * 5;
-    const end = currentPage * 5;
-
-    return [start, end];
-  }, [currentPage]);
-
-  // const [schemeList, setSchemeList] = useState([]);
 
   const HeadData = [
     "Financial Year",
@@ -38,14 +39,120 @@ const SchemeList = () => {
     },
   });
 
-  // useEffect(() => {
-  //   getAllSchemeList(userIndex).then(function (result) {
-  //     const response = result?.data?.result;
-  //     console.log(response, "res-->")
-  //     setSchemeList(response);
-  //   });
+  const ListOptions = [5, 10, 15, "all"];
+  const [items, setItems] = useState(ListOptions[0]);
 
-  // }, [])
+  const data = useMemo(() => schemeList ?? [], [schemeList]);
+
+  const list = [
+    {
+      header: "Sl no",
+      accessorKey: "scheme_sl",
+      className: "font-bold text-black text-center cursor-pointer normal-case",
+      cell: ({ row }) => row.index + 1,
+      headClass: "cursor-pointer normal-case",
+    },
+    {
+      header: "Financial Year",
+      accessorKey: "finYear",
+      headClass: "cursor-pointer normal-case",
+    },
+    {
+      header: "Block/Municipality",
+      accessorKey: "blockname",
+      headClass: "cursor-pointer normal-case",
+
+      cell: ({ row }) =>
+        row.original.blockname == ""
+          ? row.original.muniName
+          : row.original.blockname,
+      // sortingFn: "id",
+    },
+    {
+      header: "GP",
+      accessorKey: "gpName",
+      headClass: "cursor-pointer normal-case",
+      cell: ({ row }) =>
+        row.original.gpName == "" ? "-" : row.original.gpName,
+      // sortingFn: "id",
+    },
+    {
+      header: "Scheme Name",
+      accessorKey: "schemeName",
+      headClass: "cursor-pointer normal-case",
+      // sortingFn: "id",
+    },
+    {
+      header: "Project Cost",
+      accessorKey: "totalprojectCost",
+      headClass: "cursor-pointer normal-case",
+      // cell: ({ row }) => row.index + 1,
+      // sortingFn: "id",
+    },
+    {
+      header: "Total Wages Paid till date",
+      accessorKey: "totalWageCost",
+      headClass: "cursor-pointer normal-case",
+      // cell: ({ row }) => row.index + 1,
+      // sortingFn: "id",
+    },
+    {
+      header: "Sl no",
+      accessorKey: "totalLabour",
+      headClass: "cursor-pointer normal-case",
+      // cell: ({ row }) => row.index + 1,
+      // sortingFn: "id",
+    },
+    {
+      header: "Engaged For No. Of Days(MGNREGA WORKERS)",
+      headClass: "cursor-pointer normal-case",
+      className: "text-center",
+      cell: ({ row }) => "---",
+      // sortingFn: "id",
+    },
+    {
+      header: "Funding Department",
+      accessorKey: "FundingDeptname",
+      headClass: "cursor-pointer normal-case",
+      // cell: ({ row }) => row.index + 1,
+      // sortingFn: "id",
+    },
+    {
+      header: "Action",
+      headClass: "cursor-pointer normal-case",
+      className: "text-center",
+      cell: ({ row }) => "*",
+      // sortingFn: "id",
+    },
+  ];
+
+  const [sorting, setSorting] = useState([]);
+  const [filtering, setFiltering] = useState("");
+
+  const table = useReactTable({
+    data,
+    columns: list,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting: sorting,
+      globalFilter: filtering,
+    },
+    initialState: {
+      pagination: {
+        pageSize: parseInt(items),
+      },
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
+  });
+
+  useEffect(() => {
+    if (items == "all") table.setPageSize(9999);
+    else table.setPageSize(parseInt(items));
+  }, [items]);
 
   console.log(schemeList, "scehemeList");
 
@@ -86,8 +193,30 @@ const SchemeList = () => {
         </div>
       </div>
       <div className="flex flex-col flex-grow p-8 px-12">
+        <div className=" flex justify-between px-2 items-center h-12">
+          <select
+            className="rounded-lg"
+            name=""
+            id=""
+            value={items}
+            onChange={(e) => setItems(e.target.value)}
+          >
+            {ListOptions.map((e) => (
+              <option key={e} value={e}>
+                {e}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            value={filtering}
+            placeholder="search..."
+            className="border-2 rounded-lg border-zinc-400"
+            onChange={(e) => setFiltering(e.target.value)}
+          />
+        </div>
         <div className="overflow-x-auto overflow-y-hidden h-fit w-full show-scrollbar">
-          <Table className="">
+          {/* <Table className="">
             <Table.Head>
               <Table.HeadCell className="capitalize">sl no</Table.HeadCell>
               {HeadData?.map((e) => (
@@ -110,7 +239,7 @@ const SchemeList = () => {
                   <Table.Cell>
                     {d?.blockcode ? d?.blockname : d?.muniName}
                   </Table.Cell>
-                  <Table.Cell>{d?.gpName == "" ? "-" : d?.gpName }</Table.Cell>
+                  <Table.Cell>{d?.gpName == "" ? "-" : d?.gpName}</Table.Cell>
                   <Table.Cell>{d?.schemeName}</Table.Cell>
                   <Table.Cell>{d?.totalprojectCost}</Table.Cell>
 
@@ -122,8 +251,55 @@ const SchemeList = () => {
                 </Table.Row>
               ))}
             </Table.Body>
+          </Table> */}
+          <Table>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <Table.Head key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <Table.HeadCell
+                    key={header.id}
+                    className={classNames(
+                      header.column.columnDef.headClass,
+                      "hover:bg-zinc-200/70 transition-all"
+                    )}
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div className="flex items-center space-x-2">
+                        <span className="">
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                        </span>
+                        <SortIcon sort={header.column.getIsSorted()} />
+                      </div>
+                    )}
+                  </Table.HeadCell>
+                ))}
+              </Table.Head>
+            ))}
+
+            <Table.Body className="divide-y">
+              {table.getRowModel().rows.map((row) => (
+                <Table.Row key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <Table.Cell
+                      key={cell.id}
+                      className={cell.column.columnDef.className}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </Table.Cell>
+                  ))}
+                </Table.Row>
+              ))}
+            </Table.Body>
           </Table>
         </div>
+        <Pagination data={data} table={table} />
       </div>
     </>
   );
