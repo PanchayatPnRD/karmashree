c45beac653ee3909c14c2ab108b8f32bf55a8cb5
Soumyadeep Ghosh <thekillermax1999@gmail.com>
2024-07-19 14:46:31 +0530

    Update Contractor.jsx

--- src/views/forms/Contractor.jsx
+++ src/views/forms/Contractor.jsx
@@ -522,7 +522,7 @@ const Contractor = () => {
             </button>
             <button
               type="button"
-              className="w-1/6 py-2 px-4 border mt-10 border-transparent rounded-md shadow-sm text-indigo-600 border-1 border-indigo-600 bg-white hover:shadow-lg transition-all focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-1/6 py-2 px-4 border-2 mt-10 border-transparent rounded-md shadow-sm text-indigo-600 border-indigo-600 bg-white hover:shadow-lg transition-all focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={() => onSubmit("1")}
               // onClick={() => toast.success("Successfully saved as Draft")}
             >
