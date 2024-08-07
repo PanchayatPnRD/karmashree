f467d2b8f8b7643e776abd3de76711dad133a012
Animesh Chakraborty <cy.animesh@gmail.com>
2024-08-07 16:55:40 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: true,
-  },
-  plugins: [react(), mkcert()],
+ 
+  plugins: [react()],
 });
